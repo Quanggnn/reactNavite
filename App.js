8e9a52a594deb3fb474d44420b3b8751f071d4ac
quangnn <quangnnpd08348@fpt.edu.vn>
2024-01-22 21:35:39 +0700

    react

---
+++ App.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import MyLab3 from './src/csreens/lab3'
+import MyLab3_1 from './src/csreens/lab3/lab3.3'
+import MyAssignment_1 from './src/csreens/assignment'
+import Login from './src/csreens/assignment/login'
+import Register from './src/csreens/assignment/register'
+import Home from './src/csreens/assignment/home'
+import Lab4 from './src/csreens/lab4'
+const App = () => {
+  return (
+    <MyAssignment_1/>
+  )
+}
+
+export default App
---
+++ src/csreens/lab4/index.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+const Data = [
+   {
+    key: "pd001",
+    name: "Linda",
+    position: "Testter",
+    image:('../lab4/image/avta1.png')
+    
+},
+{
+    key: "pd002",
+    name: "Nguyen Van A",
+    position: "Designer"
+},
+{
+    key: "pd003",
+    name: "Tran Thi B",
+    position: "Data Entry Clerk"
+},
+{
+    key: "pd004",
+    name: "Sasa Ho",
+    position: "Sales Manager"
+},
+{
+    key: "pd005",
+    name: "Thomas Stock",
+    position: "Maketting"
+},
+{
+    key: "pd006",
+    name: "June Cha",
+    position: "Coder"
+},
+]
+const index = () => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
